1e56323eaae29c804201ea62f0a53f4a8d05bc98
Joey Yu <joey@itsjoeoui.com>
2022-11-14 19:12:49 -0500

    Setup Postgres
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -11,6 +12,16 @@ import { AppController } from './app.controller';
       autoSchemaFile: 'schema.gql',
       sortSchema: true,
       graphiql: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'example',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
     }),
     UsersModule,
     AuthModule,
